992cee09101640570dfff5182d9dbe942118644e
MosesPhilipose <mosesphilipose@gmail.com>
2025-03-29 17:41:31 +0530

    Update models.py

--- display/models.py
+++ display/models.py
@@ -1,3 +1,28 @@
+# H:\New folder (2)\display\models.py
 from django.db import models
 
-# Create your models here.
+class ReportTime(models.Model):
+    time = models.CharField(max_length=20)  # e.g., "05:20 AM"
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.time
+
+class ReportDate(models.Model):
+    date = models.CharField(max_length=50)  # e.g., "March 25, 2025"
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.date
+
+class TickerData(models.Model):
+    symbol = models.CharField(max_length=50)
+    opening_scenario = models.CharField(max_length=50)
+    trend_observed = models.CharField(max_length=50)
+    upward_close = models.FloatField()
+    downward_close = models.FloatField()
+    flat_close = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.symbol
