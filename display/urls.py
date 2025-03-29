860ca120d2de115923ab55fa235fe82453dd619d
MosesPhilipose <mosesphilipose@gmail.com>
2025-03-29 17:41:44 +0530

    Update urls.py

--- display/urls.py
+++ display/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
+
 urlpatterns = [
     path('', views.market_analysis, name='market_analysis'),
-    path('refresh-data/', views.refresh_data, name='refresh_data'),  # New endpoint
-    path('static/indexstats/data.json', views.serve_data_json, name='serve_data_json'),
+    path('refresh-data/', views.refresh_data, name='refresh_data'),
+    path('ticker-data/', views.get_ticker_data, name='ticker_data'),
 ]
-# if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
